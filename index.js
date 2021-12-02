0f3241fbeba37db7a724024fa51e6bb4cf0498c9
hosseinvejdani <vejdani.shojae@gmail.com>
2021-12-02 19:34:07 +0330

    equality operators in js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('hello taha jan!')
+
+// equality operators in js
+// (===) -> same type and same value -> Strict Equality
+// (==) -> same value only -> Lose Equality
+
+console.log( 1 === 1 ) // true
+console.log( 1 == 1 ) //true
+console.log('--------------')
+
+console.log( 1 === 1.0 ) //true
+console.log( 1 == 1.0 ) //true
+console.log('--------------')
+
+
+console.log( 1 === true ) // true
+console.log( 1 == true ) // flase
+console.log('--------------')
+
+
+console.log( 1 === '1' ) // true
+console.log( 1 == '1' ) // flase
+console.log('--------------')
