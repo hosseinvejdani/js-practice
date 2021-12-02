c9e38cbeb78d67af1f9df66a94e6deece4c35069
hosseinvejdani <vejdani.shojae@gmail.com>
2021-12-02 22:28:02 +0330

    OR(||) short circuit:

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 /*
-AND(&&) short circuit:
-In case of AND, the expression is evaluated until we get one 
-false result because the result will always be false,
-independent of the further conditions. If there is 
-an expression with &&(logical AND), and the first operand 
-itself is false, then a short circuit occurs,
-the further expression is not evaluated and false is returned.
+OR(||) short circuit: In case of OR, the expression is evaluated 
+until we get one true result because the result will 
+always be true, independent of the further conditions. 
+If there is an expression with ||(logical OR), and the 
+first operand itself is true, then a short circuit occurs, 
+evaluation stops, and false is returned.
 */
 
 
-// example 
-false && true && true && false && true // -> false (short circuit occurs)
 
-// but if we add atleast of || operand between operands:
-false && true && true && false && true || true // -> true (short circuit break!!)
-
-
+// First operand is true and operator is ||, 
+// evaluation stops and true is returned.
+console.log(true || false || false)
+  
+// Evaluation stops at the second operand(true).
+console.log(false || true || true || false)
