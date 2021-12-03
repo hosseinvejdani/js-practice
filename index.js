9eadf574fc703d8726785b51edf803078cbcb47d
hosseinvejdani <vejdani.shojae@gmail.com>
2021-12-03 15:47:05 +0330

    // primitivs vs reference types

--- index.js
+++ index.js
@@ -1,35 +1,82 @@
-// factory function
-// this is factory function 
-
-// the name of construture function set based on camel notation
-
-function createPerson(firstName, lastName) {
-   return {
-       firstName: firstName,
-       lastName: lastName,
-       getFullName() {
-           return firstName + ' ' + lastName;
-       }
-   }
-}
-
-let john = createPerson('John', 'Doe');
-
-console.log(john.getFullName());
+// primitivs vs reference types
 
 /*
-With the constructor function, you create any number of the person objects
-you want without duplicating code.
+In JavaScript, a variable may store two types of values: primitive and reference.
+
+JavaScript provides six primitive types as undefined, null, boolean, 
+number, string, and symbol , and a reference type object.
+
+The size of a primitive value is fixed, therefore, JavaScript stores 
+the primitive value on the stack.
+
+On the other hand, the size of a reference value is dynamic so JavaScript 
+stores the reference value on the heap.
+
+When you assign a value to a variable, the JavaScript engine will determine
+whether the value is a primitive or reference value.
+
+If the value is a primitive value, when you access the variable, you manipulate 
+the actual value stored in that variable. In other words, the variable that stores
+ a primitive value is accessed by value.
+
+Unlike a primitive value, when you manipulate an object, you work on the 
+reference of that object, rather than the actual object. It means a variable 
+that stores an object is accessed by reference.
+
 */
-// the name of constructor function set based on pascal notation
-function Person(firstName, lastName) {
-   this.firstName = firstName;
-   this.lastName = lastName;
-   this.fullName = function() {
-     return this.firstName + " " + this.lastName;
-   };
- }
 
- let jack = new Person('jack', 'Doe');
+// primitives are copied by thier value
+// objects are copied by thier reference
 
- console.log(jack.fullName());
+
+// example 1 :
+
+// in the case of primitives:
+let x = 10;
+let y = x;
+
+x = 5;
+
+console.log(x,y); // return 5  10
+
+// but in the case of objects:
+let obj1 = {
+  value:10
+};
+
+let obj2 = obj1;
+
+obj1.value = 5;
+
+console.log(obj1,obj2) // -> { value: 5 } { value: 5 }
+
+// primitives are copied by thier value
+// objects are copied by thier reference
+
+
+// example 2 :
+
+// in the case of primitives the value copied in argument:
+
+x = 10;
+
+function increment(num){
+  return num++;
+}
+
+increment(x)
+
+console.log(x); // -> 10
+
+
+// but in the case of objects the reference of obj are copied in argument
+// so, if we manipulate values of object in functions you should know that 
+// the original obj will be changed.
+x = {value : 10};
+
+function incrementObjectValue(obj){
+  return obj.value++;
+}
+
+incrementObjectValue(x)
+console.log(x); // -> { value: 11 }
