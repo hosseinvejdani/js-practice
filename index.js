d0012a38c2af020d6cfa31ba397e69ebe4c9617c
hosseinvejdani <vejdani.shojae@gmail.com>
2021-12-03 07:46:51 +0330

    // for in loop

--- index.js
+++ index.js
@@ -1,17 +1,91 @@
+// for in loop
 /*
-OR(||) short circuit: In case of OR, the expression is evaluated 
-until we get one true result because the result will 
-always be true, independent of the further conditions. 
-If there is an expression with ||(logical OR), and the 
-first operand itself is true, then a short circuit occurs, 
-evaluation stops, and false is returned.
+The for...of statement creates a loop iterating over iterable objects, 
+including: built-in String, Array, array-like objects (e.g., arguments
+or NodeList), TypedArray, Map, Set, and user-defined iterables.
+
+syntax is here
+
+for (variable of iterable) {
+  statement
+}
+
 */
+//============================================
+//Iterating over an Array
+const array1 = ['a', 'b', 'c'];
+
+for (const element of array1) {
+  console.log(element);
+}
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+
+//============================================
+//Iterating over a String
+const iterable = 'boo';
+
+for (const value of iterable) {
+  console.log(value);
+}
+// "b"
+// "o"
+// "o"
 
 
+//============================================
+// Iterating over a Map
+const iterable = new Map([['a', 1], ['b', 2], ['c', 3]]);
 
-// First operand is true and operator is ||, 
-// evaluation stops and true is returned.
-console.log(true || false || false)
-  
-// Evaluation stops at the second operand(true).
-console.log(false || true || true || false)
+for (const entry of iterable) {
+  console.log(entry);
+}
+// ['a', 1]
+// ['b', 2]
+// ['c', 3]
+
+for (const [key, value] of iterable) {
+  console.log(value);
+}
+// 1
+// 2
+// 3
+
+
+//============================================
+//Iterating over a Set
+const iterable = new Set([1, 1, 2, 2, 3, 3]);
+
+for (const value of iterable) {
+  console.log(value);
+}
+// 1
+// 2
+// 3
+
+
+//============================================
+// Iterating over the arguments object
+// You can iterate over the arguments object to 
+// examine all of the parameters passed into a JavaScript function:
+(function() {
+   for (const argument of arguments) {
+     console.log(argument);
+   }
+ })(1, 2, 3);
+ 
+ // 1
+ // 2
+ // 3
+
+ /*
+ Difference between for...of and for...in
+Both for...in and for...of statements iterate over something.
+The main difference between them is in what they iterate over.
+
+The for...in statement iterates over the enumerable properties of an object.
+
+The for...of statement iterates over values that the iterable object defines to be iterated over.
+ */
